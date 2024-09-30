b46d3dc96ad9da67e29c3b52bc73f6d55fc1506a
wleuenberger <leuenbe9@msu.edu>
2024-09-30 12:07:07 -0400

    Added group comparisons to function for 2 groups

--- R/ButterflyComparePostHoc.R
+++ R/ButterflyComparePostHoc.R
@@ -30,6 +30,8 @@ ButterflyComparePostHoc <- function(
     GroupList[[nn.groups]] <- WhichLists[nn.groups] %>%
       rep(times = length(SppList[[nn.groups]]))
   }
+  # Note: we don't log these because the response variable is the trends
+  # y is not the abundances (trends instead)
   y <- matrix(unlist(ThisList),
               nrow = 1000,
               ncol = NColumns)
@@ -58,5 +60,58 @@ ButterflyComparePostHoc <- function(
       '\nMeans parameterization
       \n************************************** \n')
   cat(MeanResults %>% summary)
+
+  # Pull out medians/posterior stats
+  post.hoc.df <- data.frame(trend.est = NA,
+                            trend.prob.pos = NA,
+                            trend.025 = NA,
+                            trend.25 = NA,
+                            trend.75 = NA,
+                            trend.975 = NA,
+                            Name = WhichLists)
+  # Save results and generate summary figure
+  # Calculate the probability that the trend over time is positive
+  prob.pos <- apply(MeanResults$beta.samples, 2, function(a) mean(a > 0))
+  # Save the trend into the post.hoc.df object for later
+  # Because we're using the MeanResults object, we remove the [2] from the
+  # single-species function
+  post.hoc.df$trend.prob.pos <- prob.pos
+  # Calculate quantiles of the intercept and slope terms. Median and 95% CI
+  # On real scale
+  beta.quants <- apply(MeanResults$beta.samples, 2, quantile,
+                       probs = c(0.025, 0.25, 0.5, 0.75, 0.975))
+
+  # Fill in the rest of the post.hoc.df summary
+  post.hoc.df$trend.est <- beta.quants[3,]
+  post.hoc.df$trend.025 <- beta.quants[1,]
+  post.hoc.df$trend.25 <- beta.quants[2,]
+  post.hoc.df$trend.75 <- beta.quants[4,]
+  post.hoc.df$trend.975 <- beta.quants[5,]
+
+  # Add data from the comparison model
+  if (length(WhichLists) == 2) {
+    beta.quants <- apply(ComparedResults$beta.samples,
+                         2,
+                         quantile,
+                         probs = c(0.025, 0.25, 0.5, 0.75, 0.975))
+    post.hoc.df %<>%
+      add_row(
+        Name = paste(WhichLists[1], '-', WhichLists[2]),
+        trend.prob.pos = apply(ComparedResults$beta.samples, 2, function(a)
+          mean(a > 0))[2],
+        trend.est = beta.quants[3, 2],
+        trend.025 = beta.quants[1, 2],
+        trend.25 = beta.quants[2, 2],
+        trend.75 = beta.quants[4, 2],
+        trend.975 = beta.quants[5, 2]
+      )
+  } # WhicLists == 2
+
+  ReturnList <- list('Data' = new.data.list,
+                     'Compared' = ComparedResults,
+                     'Mean' = MeanResults,
+                     'SummaryStats' = post.hoc.df)
+
+
   return(ReturnList)
 }
